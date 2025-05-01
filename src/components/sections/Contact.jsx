d7e68ce9f54887086fb60f63359d852bd5e3c688
Daniel Purwandi <daanielnp@Daniels-MacBook-Pro.local>
2025-05-01 23:54:43 +0700

    first commit

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -22,7 +22,7 @@ export const Contact = () => {
     return(
         <section id="contact" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
-                <div className="px-4 w-250 max-w-4xl mx-auto">
+                <div className="px-4 w-full md:w-150">
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">Get In Touch</h2>
                     <form className="space-y-6" onSubmit={handleSubmit}>
                         <div className="relative">
